0a8a52e222de4110ff66d071f13ca87c1bc34c5d
arayama <sinzan062871@gmail.com>
2018-09-23 19:11:24 +0900

    Metalクラスの変換が負荷でコンパイルエラー

--- raytracing_cpp/rayt.cpp
+++ raytracing_cpp/rayt.cpp
@@ -1,34 +1,41 @@
 #include "pch.h"
-#include "stb.h"
+#include "rayt.h"
+
+float hit_sphere(const vec3& center, float radius, const rayt::Ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0f * dot(r.direction(), oc);
+	float c = dot(oc, oc) - pow2(radius);
+	float D = b * b - 4 * a*c;
+	if (D < 0) {
+		return -1.0f;
+	}
+	else {
+		return (-b - sqrtf(D)) / (2.0f*a);
+	}
+}
 
 vec3 color(const rayt::Ray& r) {
+	vec3 c(0, 0, -1);
+	float t = hit_sphere(c, 0.5f, r);
+	if (t > 0.0f) {
+		vec3 N = normalize(r.at(t) - c);
+		return 0.5f*(N + vec3(1.0f));
+	}
 	vec3 d = normalize(r.direction());
-	float t = 0.5f * (r.direction().getY() + 1.0f);
-	return lerp(t, vec3(0.5f, 0.7f, 1.0f), vec3(1));
+	t = 0.5f*(r.direction().getY() + 1.0f);
+	return lerp(t, vec3(1), vec3(0.5f, 0.7f, 1.0f));
 }
+
+
 
 int main()
 {
-	int nx = 200;
-	int ny = 100;
-	std::unique_ptr<rayt::Image> image(std::make_unique<rayt::Image>(nx, ny));
-
-	vec3 x(4.0f, 0.0f, 0.0f);
-	vec3 y(0.0f, 2.0f, 0.0f);
-	vec3 z(-2.0f, -1.0f, -1.0f);
-	std::unique_ptr<rayt::Camera> camera(std::make_unique<rayt::Camera>(x, y, z));
-
-	for (int j = 0; j < ny; ++j) {
-		std::cerr << "Rendering (y = " << j << ") " << (100.0 * j / (ny - 1)) << "%" << std::endl;
-		for (int i = 0; i < nx; ++i) {
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			rayt::Ray r = camera->getRay(u, v);
-			vec3 c = color(r);
-			image->write(i, j, c.getX(), c.getY(), c.getZ());
-		}
-	}
-	stbi_write_bmp("render.bmp", nx, ny, sizeof(rayt::Image::rgb), image->pixels());
+	int nx = 800;
+	int ny = 400;
+	int ns = 50;
+	std::unique_ptr<rayt::Scene> scene(std::make_unique<rayt::Scene>(nx, ny, ns));
+	scene->render();
 
 	system("PAUSE");
 	return 0;
--- raytracing_cpp/rayt.h
+++ raytracing_cpp/rayt.h
@@ -1,10 +1,5 @@
 #pragma once
-#include <memory>
-#include <cstdlib>
-#include <iostream>
-#include <vectormath/scalar/cpp/vectormath_aos.h>
-#include <float.h>  // FLT_MIN, FLT_MAX
-#include <random>
+#include "stb.h"
 
 #define PI 3.14159265359f
 #define PI2 6.28318530718f
@@ -13,6 +8,7 @@
 #define LOG2 1.442695f
 #define EPSILON 1e-6f
 #define GAMMA_FACTOR 2.2f
+#define NUM_THREAD 8
 
 inline float drand48() {
 	return float(((double)(rand()) / (RAND_MAX))); /* RAND_MAX = 32767 */
@@ -36,6 +32,114 @@ typedef Vectormath::Aos::Vector3 col3;
 
 namespace rayt {
 
+
+	class Shape;
+	typedef std::shared_ptr<Shape> ShapePtr;
+	class Material;
+	typedef std::shared_ptr<Material> MaterialPtr;
+
+	inline vec3 random_vector() {
+		return vec3(drand48(), drand48(), drand48());
+	}
+
+	inline vec3 random_in_unit_sphere() {
+		vec3 p;
+		do {
+			p = 2.f * random_vector() - vec3(1.f);
+		} while (lengthSqr(p) >= 1.f);
+		return p;
+	}
+
+	inline vec3 reflect(const vec3& v, const vec3& n) {
+		return v - 2.f * dot(v, n)*n;
+	}
+
+	class Ray {
+	public:
+		Ray() {}
+		Ray(const vec3& o, const vec3& dir)
+			: m_origin(o)
+			, m_direction(dir) {
+		}
+
+		const vec3& origin() const { return m_origin; }
+		const vec3& direction() const { return m_direction; }
+		vec3 at(float t) const { return m_origin + t * m_direction; }
+
+	private:
+		vec3 m_origin;    // 始点
+		vec3 m_direction; // 方向（非正規化）
+	};
+
+	inline vec3 linear_to_gamma(const vec3& v, float gammaFactor) {
+		float recipGammaFactor = recip(gammaFactor);
+		return vec3(
+			powf(v.getX(), recipGammaFactor),
+			powf(v.getY(), recipGammaFactor),
+			powf(v.getZ(), recipGammaFactor));
+	}
+
+	inline vec3 gamma_to_linear(const vec3& v, float gammaFactor) {
+		return vec3(
+			powf(v.getX(), gammaFactor),
+			powf(v.getY(), gammaFactor),
+			powf(v.getZ(), gammaFactor));
+	}
+
+	class ImageFilter {
+	public:
+		virtual vec3 filter(const vec3& c) const = 0;
+	};
+
+	class GammaFilter : public ImageFilter {
+	public:
+		GammaFilter(float factor) : m_factor(factor) {}
+		virtual vec3 filter(const vec3& c) const override {
+			return linear_to_gamma(c, m_factor);
+		}
+	private:
+		float m_factor;
+	};
+
+	class Image {
+	public:
+		struct rgb {
+			unsigned char r;
+			unsigned char g;
+			unsigned char b;
+		};
+
+		Image() : m_pixels(nullptr) { }
+
+		Image(int w, int h) {
+			m_width = w;
+			m_height = h;
+			m_pixels.reset(new rgb[m_width * m_height]);
+			m_filters.push_back(std::make_unique<GammaFilter>(GAMMA_FACTOR));
+		}
+
+		int width() const { return m_width; }
+		int height() const { return m_height; }
+		void* pixels() const { return m_pixels.get(); }
+
+		void write(int x, int y, float r, float g, float b) {
+			vec3 c(r, g, b);
+			for (auto& f : m_filters) {
+				c = f->filter(c);
+			}
+			int index = m_width * y + x;
+			m_pixels[index].r = static_cast<unsigned char>(c.getX() * 255.99f);
+			m_pixels[index].g = static_cast<unsigned char>(c.getY() * 255.99f);
+			m_pixels[index].b = static_cast<unsigned char>(c.getZ() * 255.99f);
+		}
+
+	private:
+		int m_width;
+		int m_height;
+		std::unique_ptr<rgb[]> m_pixels;
+		std::vector< std::unique_ptr<ImageFilter> > m_filters;
+	};
+
 	class Camera {
 	public:
 		Camera() {}
@@ -58,7 +162,7 @@ namespace rayt {
 			m_uvw[1] = 2.0f * halfH * v;
 		}
 
-		Ray getRay(float u, float v) const{
+		Ray getRay(float u, float v) const {
 			return Ray(m_origin, m_uvw[2] + m_uvw[0] * u + m_uvw[1] * v - m_origin);
 		}
 
@@ -67,52 +171,234 @@ namespace rayt {
 		vec3 m_uvw[3];  // 直交基底ベクトル
 	};
 
-	class Ray {
+	class HitRec {
 	public:
-		Ray() {}
-		Ray(const vec3& o, const vec3& dir)
-			: m_origin(o)
-			, m_direction(dir) {
-		}
-
-		const vec3& origin() const { return m_origin; }
-		const vec3& direction() const { return m_direction; }
-		vec3 at(float t) const { return m_origin + t * m_direction; }
-
-	private:
-		vec3 m_origin;    // 始点
-		vec3 m_direction; // 方向（非正規化）
+		float t;
+		vec3 p;
+		vec3 n;
+		MaterialPtr mat;
 	};
 
-	class Image {
+	class Shape {
 	public:
-		struct rgb {
-			unsigned char r;
-			unsigned char g;
-			unsigned char b;
+		virtual bool hit(const Ray& r, float t0, float t1, HitRec& hrec) const = 0;
+	};
+
+	class ScatterRec {
+	public:
+		Ray ray;
+		vec3 albedo;
+	};
+
+	class Material {
+	public:
+		virtual bool scatter(const Ray& r, const HitRec& hrec, ScatterRec& srec) const = 0;
+	};
+
+	class Lambertian : public Material {
+	public:
+		Lambertian(const vec3& c)
+			: m_albedo(c) {
+		}
+
+		virtual bool scatter(const Ray& r, const HitRec& hrec, ScatterRec& srec) const override {
+			vec3 target = hrec.p + hrec.n + random_in_unit_sphere();
+			srec.ray = Ray(hrec.p, target - hrec.p);
+			srec.albedo = m_albedo;
+			return true;
 		};
 
-		Image() : m_pixels(nullptr) { }
-		Image(int w, int h) {
-			m_width = w;
-			m_height = h;
-			m_pixels.reset(new rgb[m_width*m_height]);
+	private:
+		vec3 m_albedo;
+	};
+
+	class Metal : public Material {
+	public:
+		Metal(const vec3& c, float fuzz)
+			: m_albedo(c) {
 		}
 
-		int width() const { return m_width; }
-		int height() const { return m_height; }
-		void* pixels() const { return m_pixels.get(); }
-
-		void write(int x, int y, float r, float g, float b) {
-			int index = m_width * y + x;
-			m_pixels[index].r = static_cast<unsigned char>(r*255.99f);
-			m_pixels[index].g = static_cast<unsigned char>(g*255.99f);
-			m_pixels[index].b = static_cast<unsigned char>(b*255.99f);
+		virtual bool scatter(const Ray& r, const HitRec& hrec, ScatterRec& srec) const override {
+			vec3 reflected = reflect(normalize(r.direction()), hrec.n);
+			srec.ray = Ray(hrec.p, reflected);
+			srec.albedo = m_albedo;
+			return dot(srec.ray.direction(), hrec.n) > 0;
 		}
 
 	private:
-		int m_width;
-		int m_height;
-		std::unique_ptr<rgb[]> m_pixels;
+		vec3 m_albedo;
 	};
+
+	
+
+	class Sphere : public Shape {
+	public:
+		Sphere() {}
+		Sphere(const vec3& c, float r,const MaterialPtr& mat) : m_center(c), m_radius(r),m_material(mat) {}
+		virtual bool hit(const Ray& r, float t0, float t1, HitRec& hrec) const override {
+			vec3 oc = r.origin() - m_center;
+			float a = dot(r.direction(), r.direction());
+			float b = 2.0f*dot(oc, r.direction());
+			float c = dot(oc, oc) - pow2(m_radius);
+			float D = b * b - 4 * a*c;
+			if (D > 0) {
+				float root = sqrtf(D);
+				float temp = (-b - root) / (2.0f*a);
+				if (temp < t1 && temp > t0) {
+					hrec.t = temp;
+					hrec.p = r.at(hrec.t);
+					hrec.n = (hrec.p - m_center) / m_radius;
+					hrec.mat = m_material;
+					return true;
+				}
+				temp = (-b + root) / (2.0f*a);
+				if (temp < t1 && temp > t0) {
+					hrec.t = temp;
+					hrec.p = r.at(hrec.t);
+					hrec.n = (hrec.p - m_center) / m_radius;
+					hrec.mat = m_material;
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+	private:
+		vec3 m_center;
+		float m_radius;
+		MaterialPtr m_material;
+	};
+
+	class ShapeList : public Shape {
+	public:
+		ShapeList() {}
+
+		void add(const ShapePtr& shape) {
+			m_list.push_back(shape);
+		}
+
+		virtual bool hit(const Ray& r, float t0, float t1, HitRec& hrec) const override {
+			HitRec temp_rec;
+			bool hit_anything = false;
+			float closest_so_far = t1;
+			for (auto& p : m_list) {
+				if (p->hit(r, t0, closest_so_far, temp_rec)) {
+					hit_anything = true;
+					closest_so_far = temp_rec.t;
+					hrec = temp_rec;
+				}
+			}
+			return hit_anything;
+		}
+
+	private:
+		std::vector<ShapePtr> m_list;
+	};
+
+
+	class Scene {
+	public:
+		Scene(int width, int height, int samples)
+			: m_image(std::make_unique<Image>(width, height))
+			, m_backColor(0.2f) 
+			, m_samples(samples){
+		}
+
+		void build() {
+
+			// Camera
+
+			vec3 w(-2.0f, -1.0f, -1.0f);
+			vec3 u(4.0f, 0.0f, 0.0f);
+			vec3 v(0.0f, 2.0f, 0.0f);
+			m_camera = std::make_unique<Camera>(u, v, w);
+
+			// Shapes 
+			ShapeList* world = new ShapeList();
+			world->add(std::make_shared<Sphere>(
+				vec3(0.6, 0, -1), 0.5f,
+				std::make_shared<Lambertian>(vec3(0.1f, 0.2f, 0.5f))));
+			world->add(std::make_shared<Sphere>(
+				vec3(-0.6, 0, -1), 0.5f,
+				std::make_shared<Metal>(vec3(0.8f, 0.8f, 0.8f))));
+			world->add(std::make_shared<Sphere>(
+				vec3(0, -100.5, -1), 100,
+				std::make_shared<Lambertian>(vec3(0.8f, 0.8f, 0.0f))));
+			m_world.reset(world);
+		}
+
+		float hit_sphere(const vec3& center, float radius, const rayt::Ray& r) const {
+			vec3 oc = r.origin() - center;
+			float a = dot(r.direction(), r.direction());
+			float b = 2.0f * dot(r.direction(), oc);
+			float c = dot(oc, oc) - pow2(radius);
+			float D = b * b - 4 * a*c;
+			if (D < 0) {
+				return -1.0f;
+			}
+			else {
+				return (-b - sqrtf(D)) / (2.0f*a);
+			}
+		}
+
+		vec3 background(const vec3& d) const {
+			return m_backColor;
+		}
+
+		vec3 backgroundSky(const vec3& d) const {
+			vec3 v = normalize(d);
+			float t = 0.5f * (v.getY() + 1.0f);
+			return lerp(t, vec3(1), vec3(0.5f, 0.7f, 1.0f));
+		}
+
+		vec3 color(const rayt::Ray& r, const Shape* world) const {
+			HitRec hrec;
+			if (world->hit(r, 0.001f, FLT_MAX, hrec)) {
+				ScatterRec srec;
+				if (hrec.mat->scatter(r, hrec, srec)) {
+					return mulPerElem(srec.albedo, color(srec.ray, world));
+				}
+				else {
+					return vec3(0);
+				}
+			}
+			return backgroundSky(r.direction());
+		}
+
+		void render() {
+
+			build();
+
+			int nx = m_image->width();
+			int ny = m_image->height();
+#pragma omp parallel for schedule(dynamic, 1) num_threads(NUM_THREAD)
+			for (int j = 0; j < ny; ++j) {
+				std::cerr << "Rendering (y = " << j << ") " << (100.0 * j / (ny - 1)) << "%" << std::endl;
+				for (int i = 0; i < nx; ++i) {
+					vec3 c(0);
+					for (int s = 0; s < m_samples; ++s) {
+						float u = (float(i) + drand48()) / float(nx);
+						float v = (float(j) + drand48()) / float(ny);
+						Ray r = m_camera->getRay(u, v);
+						c += color(r, m_world.get());
+					}
+
+					c /= m_samples;
+					m_image->write(i, (ny - j - 1), c.getX(), c.getY(), c.getZ());
+				}
+			}
+
+			stbi_write_bmp("render.bmp", nx, ny, sizeof(Image::rgb), m_image->pixels());
+		}
+
+	private:
+		std::unique_ptr<Camera> m_camera;
+		std::unique_ptr<Image> m_image;
+		vec3 m_backColor;
+		std::unique_ptr<Shape> m_world;
+		int m_samples;
+	};
+
+	
 } // namespace rayt
